e0a60050c9ad9923177d30c8ecb1394bb1a3ed9e
danielasamardgieva <samardzhievad@gmail.com>
2015-11-03 20:41:39 +0200

    Модул 8  Видео 3 Четене на текстов файл - не чете

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -11,17 +11,39 @@ namespace IOTextFiles
 			_stable = stable;	
 		}
 
+		public  string getPath()
+		{
+			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test");
+
+			return _path;
+		}
 		public bool save()
 		{
-			try {
-				string _temp = "";
-
-				_temp = String.Join ("\r\n", _stable.stable);
+			try
+			{string _temp="";
+				_temp=String.Join("\r\n", _stable.stable);
 
 				//Запис на текстов файл
-				System.IO.File.WriteAllText ("c:\\aula\\test.txt", _temp);
+				System.IO.File.WriteAllText(getPath(), _temp);
 
 				return true;
+
+			} catch {
+			}
+
+			return false;
+		}
+		public bool open()
+		{
+			try {
+				string _temp = System.IO.File.ReadAllText (getPath ());
+				string[] _table = _temp.Replace ("\r", "").Split ('\n');
+
+				for (int i = 0; i < _table.Length; i++) {
+					_stable.stable [i] = _table [i];
+				}
+				return true;
+
 			} catch {
 			}
 			return false;
--- IOTesting/IOTextFiles/STable.cs
+++ IOTesting/IOTextFiles/STable.cs
@@ -15,7 +15,7 @@ namespace IOTextFiles
 		{
 			stable[0]="1,0,5,0.45";
 			stable[1]="2,5,10,0.45";
-			stable[2]="3,10,15,0.45";
+			stable[2]="3,10,15,0.455555";
 			stable[3]="4,15,20,0.45";
 		}
 	}
